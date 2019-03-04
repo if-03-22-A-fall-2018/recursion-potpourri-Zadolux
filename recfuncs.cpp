a11cbf8947c025c561e667e55cb171f43fb7f399
Zadolux <25794895+Zadolux@users.noreply.github.com>
2019-03-04 23:14:45 +0100

    Builds

---
+++ recfuncs.cpp
@@ -0,0 +1,37 @@
+#include "recfuncs.h"
+
+struct CalculationResults perform_calculations(int* arr, int n)
+{
+  struct CalculationResults calc_results;
+  return calc_results;
+}
+
+int* create_array(int size)
+{
+  return 0;
+}
+
+int calc_array_size(int n)
+{
+  return 0;
+}
+
+int calc_factorial(int n)
+{
+  return 0;
+}
+
+void calc_sums(int* n, int count, int* out_arr, int* out_arr_bin)
+{
+
+}
+
+int convert_to_binary(int n)
+{
+  return 0;
+}
+
+int thx(int* nums, int size, int* out_arr, int* out_arr_bin, int i, int index, int sum)
+{
+  return 0;
+}
--- recfuncs.h
+++ recfuncs.h
@@ -1,6 +1,20 @@
 #ifndef ___RECURSIVE_POTPOURRI
 #define ___RECURSIVE_POTPOURRI
 
+struct CalculationResults {
+    int* sums;
+    int* binary_sums;
+    int* num_factorials;
+    int no_of_nums;
+    int no_of_sums;
+};
 
+struct CalculationResults perform_calculations(int* arr, int n);
+int* create_array(int size);
+int calc_array_size(int n);
+int calc_factorial(int n);
+void calc_sums(int* n, int count, int* out_arr, int* out_arr_bin);
+int convert_to_binary(int decimal);
+int thx(int* nums, int size, int* out_arr, int* out_arr_bin, int i, int index, int sum);
 
 #endif
